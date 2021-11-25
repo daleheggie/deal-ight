9a7f23cdbcf8f5fb6fd43d0f4e47e2dbed71a97b
dale.heggie <dale.heggie@hotmail.com>
2021-11-24 19:06:33 -0800

    Created seed data files

---
+++ server/seed_data/deals.js
@@ -0,0 +1,39 @@
+// dummy deals to populate database for testing
+module.exports = [
+    {
+        id: 1,
+        day: ['tuesday'],
+        until: new Date(2022, 0, 6),
+        establishment_id: 1,
+        details: 'Dinner for 2 for $30, includes 2 mains and a bottle of wine plus dessert',
+        likes: 0,
+        dislikes: 0
+    },
+    {
+        id: 2,
+        day: ['wednesday', 'monday'],
+        until: new Date(2021, 11, 25),
+        establishment_id: 2,
+        details: 'Any burger and any pint for $15',
+        likes: 0,
+        dislikes: 0
+    },
+    {
+        id: 3,
+        day: ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday'],
+        until: new Date(2022, 2, 6),
+        establishment_id: 3,
+        details: '5 courses for $50, wine pairings for an additional $18',
+        likes: 0,
+        dislikes: 0
+    },
+    {
+        id: 4,
+        day: ['thursday'],
+        until: new Date(2021,11,31),
+        establishment_id: 3,
+        details: 'All gin and tonics $7 singles $9 doubles and margharita pitchers $20',
+        likes: 0,
+        dislikes: 0
+    }
+]
---
+++ server/seed_data/users.js
@@ -0,0 +1,24 @@
+// dummy users to populate database for testing
+module.exports = [
+    {
+        id: 1,
+        username: 'testUser1',
+        name: 'Dale Heggie',
+        favourite_restaurant_Id: [1,3],
+        favourite_deal_id: []
+    },
+    {
+        id: 2,
+        username: 'testUser2',
+        name: 'Nolan Knill',
+        favourite_restaurant_Id: [1],
+        favourite_deal_id: [1]
+    },
+    {
+        id: 3,
+        username: 'testUser3',
+        name: 'Nicholas Macdonald',
+        favourite_restaurant_Id: [2],
+        favourite_deal_id: [3]
+    }
+]
