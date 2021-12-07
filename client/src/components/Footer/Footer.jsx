babeebf49723424eec1403db74e940f229460d4d
dale.heggie <dale.heggie@hotmail.com>
2021-12-07 11:52:40 -0800

    Fixed footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,19 @@
+import './Footer.scss'
+
 const Footer = () => {
     return(
-        <section>
-            <h3>Developer</h3>
+        <section className='footer'>   
             <div className='footer__name'>
+                <h3>Contact the developer</h3>
                 <p>Dale Heggie</p>
             </div>
             <div className='footer__phone'>
-                <p>+1 (604)-388-4367</p>
+                <img className='footer__icon' src='http://localhost:5000/icons/call-icon.png' alt='phone icon'/>
+                <a className='footer__link' href='tel:+16043884367'>+1 (604)-388-4367</a>
             </div>
             <div className='footer__email'>
-                <p>dale.heggie@hotmail.com</p>
+                <img className='footer__icon' src='http://localhost:5000/icons/email-icon.png' alt='email icon'/>  
+                <a className='footer__link' href='mailto:dale.heggie@hotmail.com'>dale.heggie@hotmail.com</a>
             </div>
         </section>
     )
